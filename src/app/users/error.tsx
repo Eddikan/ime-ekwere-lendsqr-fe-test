10f3311979589512a20fd04f736f5c00a0d09907
eddikan <imeekwere15@gmail.com>
2024-10-31 09:34:34 +0100

    fix: unused var error

--- src/app/users/error.tsx
+++ src/app/users/error.tsx
@@ -6,7 +6,7 @@ import React from "react";
 const ErrorComponent = ({ error, reset }: { error: Error; reset: () => void }) => {
   return (
     <div>
-      <h2>Something went wrong with the User page.</h2>
+      <h2>Something went wrong with the User {error.message} page.</h2>
       <button onClick={reset}>Try Again</button>
     </div>
   );
