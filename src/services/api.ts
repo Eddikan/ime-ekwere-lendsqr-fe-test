c8679e0f26afcc9e9763a2caddb32e310c3e98a8
eddikan <imeekwere15@gmail.com>
2024-11-06 12:57:02 +0100

    fix: vercel deployment error

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,16 +1,8 @@
 // api.ts
 import axios from 'axios';
+import { UserType } from "@/interfaces/users";
 
 // Define the shape of your user data
-export interface UserData {
-    id: string;
-    organization: string;
-    username: string;
-    email: string;
-    phoneNumber: string;
-    dateJoined: string;
-    status: string;
-}
 
 // Create an axios instance
 const api = axios.create({
@@ -21,7 +13,7 @@ const api = axios.create({
 });
 
 // Fetch data function
-export const fetchData = async (): Promise<UserData[]> => {
-    const response = await api.get<UserData[]>('/data'); // Use the type for the response
+export const fetchData = async (): Promise<UserType[]> => {
+    const response = await api.get<UserType[]>('/data'); // Use the type for the response
     return response.data;
 };
